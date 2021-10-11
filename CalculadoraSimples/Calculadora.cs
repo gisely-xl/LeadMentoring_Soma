3936779c3b090938611c9ba6b9b6000ae152bfea
Gisely Vasconcelos <83426796+gisely-xl@users.noreply.github.com>
2021-10-10 21:08:13 -0300

    First Commit

---
+++ CalculadoraSimples/Calculadora.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace CalculadoraSimples
+{
+    class Calculadora
+    {
+        public double soma, sub, mult, div;
+
+
+
+        public void Soma(double num1, double num2) 
+        {
+            soma = num1 + num2;
+            Console.WriteLine("Soma: " + soma);
+        }
+        public void Sub(double num1, double num2)
+        {
+            sub = num1 - num2;
+            Console.WriteLine("Subtração: " + sub);
+        }
+        public void Div(double num1, double num2)
+        {
+            if(num2 != 0)
+            {
+                div = num1 / num2;
+                Console.WriteLine("Divisão: " + div.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            else
+            {
+                Console.WriteLine("\nDivisão não efetuada. O denominador não pode ser 0.\n");
+            }
+        }
+        public void Mult(double num1, double num2)
+        {
+            mult = num1 * num2;
+            Console.WriteLine("Multiplicação: " + mult);
+        }
+
+    }
+}
---
+++ CalculadoraSimples/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CalculadoraSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora op = new Calculadora();
+            Console.Write("Digite o primeiro número: ");
+            double num1 = double.Parse(Console.ReadLine());
+            Console.Write("Digite o segundo numero: ");
+            double num2 = double.Parse(Console.ReadLine());
+
+            op.Soma(num1, num2);//posso chamar quantas vezes eu quiser
+            op.Sub(num1, num2);
+            op.Div(num1, num2);
+            op.Mult(num1, num2);
+
+        }
+    }
+}
